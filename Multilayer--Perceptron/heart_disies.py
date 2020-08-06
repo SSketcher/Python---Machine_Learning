ec823ad7244361c367962260639ca5ca670c97d4
SSketcher <45407041SSketcher@users.noreply.github.com>
2020-08-06 15:06:56 +0200

    cost function

--- Multilayer--Perceptron/heart_disies.py
+++ Multilayer--Perceptron/heart_disies.py
@@ -1,4 +1,16 @@
+from perceptron import Perceptron, Layer
 import numpy as np
 import pandas as pd
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data')
+data = df.iloc[:, :13].values
+labels = df.iloc[:, 13:].values
+labels = np.reshape(labels, (len(labels), 1))
+
+data[data == '?'] = -1      #marking valeues '?' as -1
+
+model = Perceptron([
+    Layer(size = len(data[0])),
+    Layer(size = 8),
+    Layer(size = 8),
+    Layer(size = 4)], activation = 'sigmoid')
--- Multilayer--Perceptron/perceptron.py
+++ Multilayer--Perceptron/perceptron.py
@@ -21,11 +21,17 @@ class Perceptron(object):
             acc.append((y, yp))
         return self.__accuracy(acc)
 
-    def predict(self,X: np.array):
+    def predict(self, X: np.array):
         for layer in self.layers[1:]:
             Xi = np.dot(X, layer.weights[:][1:]) - layer.weights[:][0]
-            X = self.__activation_fnc(Xi)
-        return  X
+            output = self.__activation_fnc(Xi)
+        return  output
+
+    def __gradient(self,X: np.array, Y: np.array, Yp: np.array):
+        n = len(self.layers) - 2
+        for i in range(n):
+            deltJ = np.multiply(-(Y - Yp), self.__activation_fnc_div())
+            dJdW = np.dot()
 
     def __activation_fnc(self,X):     #function applying selected activation function
         if self.activation == 'relu':
@@ -35,14 +41,31 @@ class Perceptron(object):
         elif self.activation == 'heaviside':
             return self.__heaviside(X)
 
+    def __activation_fnc_div(self,X):     #function applying selected activation function
+        if self.activation == 'relu':
+            return self.__relu_div(X)
+        elif self.activation == 'sigmoid':
+            return self.__sigmoid_div(X)
+        elif self.activation == 'heaviside':
+            return self.__heaviside_div(X)
+
     def __relu(self, X: np.array):     #rectifier activation function
         return np.log((1 + np.exp(X)), np.e)
+
+    def __relu_div(self, X: np.array):
+        return 1 / (1 + np.exp(-X))
 
     def __sigmoid(self, X: np.array):       #sigmoid activation function
         return 1 / (1 + np.exp(-X))
 
+    def __sigmoid_div(self, X: np.array):
+        return np.exp(X) / (1 + np.exp(-X))
+
     def __heaviside(self, X: np.array):     #heaviside activation function
         return np.where(X >= 0, 1, 0)
+
+    def __heaviside_div(self, X: np.array):
+        return np.where(X == 0, 1, 0)
 
     def __randomize(self, data: np.array, labels: np.array):        #function shuffling dataset
         df = np.hstack((data, labels))
@@ -50,8 +73,10 @@ class Perceptron(object):
         df = np.hsplit(df, np.array([len(data[0]), len(data)]))
         return (df[0], df[1])
 
-    def __cost_func(self):
-        pass
+    def __cost_func(self, acc: list()):       #calculating valeu of cost function
+        y = np.array(acc[:][0])
+        yp = np.array(acc[:][1])
+        return sum(o.5 * (y - yp))
 
     def __accuracy(self,acc: list()):       #calculating accuracy of predictions
         total_population = len(acc)
@@ -70,7 +95,7 @@ class Perceptron(object):
 class Layer(object):
     def __init__(self,size = 1):
         self.size = size
-        self.weights
+        self.weights = None
 
-    def set_weights(self, prev_layer: Layer):
+    def set_weights(self, prev_layer):
         self.weights = np.zeros((self.size, prev_layer.size + 1))

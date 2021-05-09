ad6cce64186e198054c7cf9f6680e607f8534734
SSketcher <kubul10@op.pl>
2021-05-09 13:24:33 +0200

    update
    Updating the comments with some minor changes in code

--- Univariate_Linear_Regression/test_example.py
+++ Univariate_Linear_Regression/test_example.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
+#Creating dataset
 eps = np.random.randint(5, size=50)/2
 x = np.linspace(1.0, 50.0, num = 50)
 y = 3/4 * x + 2
@@ -11,6 +12,7 @@ X = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 Y = np.array([3.5, 2.0, 5.0 , 2.5, 6.0 , 7.2, 5.4 , 8, 7.5, 11.2, 10.5, 13.4, 14.0, 13.0, 16.5])
 
 
+#Ploting dataset
 plt.plot(X, Y, 'gh')
 plt.xlabel('x')
 plt.ylabel('y')
@@ -19,11 +21,14 @@ plt.grid(True)
 plt.show()
 
 
-model = Regression()
-model.fit(X, Y, epochs = 50)
+#Performing linear regression
+model = Regression()      #Declaration of the model
+model.fit(X, Y, epochs = 50)        #Training the model
 
-Yp = model.predict(X)
+Yp = model.predict(X)        #Testing the model
 
+
+#Ploting dataset with change of regresion line
 plt.plot(X, Y, 'gh')
 for epoch in model.log:
     line = epoch[1][0] * X + epoch[1][1]
@@ -36,11 +41,11 @@ plt.grid(True)
 plt.show()
 
 
-
+#Plotting change of cost function value by the epochs
 x = np.linspace(1.0, 50.0, num = 50)
 cost = [element[0] for element in model.log]
 
-plt.plot(x, cost, "bd")
+plt.plot(x, cost)
 plt.xlabel('Epochs')
 plt.ylabel('Cost function')
 plt.title('Cost function for each epoch')
